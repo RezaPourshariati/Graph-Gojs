767c8bf19e0525349c7b8432199ffd021a7d8aa5
رضا پورشریعتی <rpourshariati@noornent.net>
2024-07-09 13:35:53 +0330

    added graph-data and overview map

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {onMounted} from "vue";
 import go from 'gojs'
+import {hadithData, relationHadithData} from './components/graph-data/graphData3080.js'
 
 onMounted(() => {
   const myTag = document.getElementById('myDiagramDiv')
@@ -17,33 +18,33 @@ onMounted(() => {
       .add(new go.Shape("RoundedRectangle",
           {strokeWidth: 0, fill: "white"})  // no border; default fill is white
           .bind("fill", "color"))  // Shape.fill is bound to Node.data.color
-      .add(new go.TextBlock({margin: 8, stroke: "#333"})  // some room around the text
-          .bind("text", "key"));  // TextBlock.text is bound to Node.data.key
-
+      .add(new go.TextBlock({margin: 8, stroke: "#333", font: "bold 14pt sans-serif"})  // some room around the text
+          .bind("text", "hadith"))  // TextBlock.text is bound to Node.data.key
   // but use the default Link template, by not setting Diagram.linkTemplate
 
   // create the model data that will be represented by Nodes and Links
-  myDiagram.model = new go.GraphLinksModel(
-      [
-        {key: "Alpha", color: "lightblue"},
-        {key: "Beta", color: "orange"},
-        {key: "Gamma", color: "lightgreen"},
-        {key: "Delta", color: "pink"}
-      ],
-      [
-        {from: "Alpha", to: "Beta"},
-        {from: "Alpha", to: "Gamma"},
-        {from: "Beta", to: "Beta"},
-        {from: "Gamma", to: "Delta"},
-        {from: "Delta", to: "Alpha"}
-      ]);
+  myDiagram.model = new go.GraphLinksModel(hadithData, relationHadithData)
+
+  const myOverview =
+      new go.Overview("myOverviewDiv",
+          { observed: myDiagram });
 })
 </script>
 
+
 <template>
-  <div id="myDiagramDiv" style="width:100%; height:100vh; border: 5px solid green"></div>
+  <div id="myDiagramDiv" style="width:1000px; height:80vh;"></div>
+  <div id="myOverviewDiv" style="width:150px; height:150px; border: 5px solid orangered"></div>
 </template>
 
-<style scoped>
 
+<style scoped>
+#myDiagramDiv {
+  border: 4px solid green;
+  margin: 0 auto;
+}
+
+#myOverviewDiv {
+  margin: 0 auto;
+}
 </style>
