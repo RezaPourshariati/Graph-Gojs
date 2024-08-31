41196fd186ed32d498ac1cc4e0c4257df1afad06
رضا پورشریعتی <rpourshariati@noornent.net>
2024-08-31 15:03:18 +0330

    sub-feat(client): crated none-virtual tree layout with navbar for controlling nodes

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,18 @@
 import 'primeflex/primeflex.css'
 import 'primeflex/themes/primeone-light.css'
 // import HadithGraph from '@/component/HadithGraph.vue'
-import AnsabTreeGraphLayout from "@/component/AnsabTreeGraphLayout.vue";
+// import AnsabTreeGraphLayout from "@/component/AnsabTreeGraphLayout.vue";
 // import AnsabTreeGraph from "@/component/AnsabTreeGraph.vue";
 // import NoorSirehExample from "@/component/NoorSirehExample.vue";
 // import NoorSirehExampleOld from "@/component/NoorSirehExampleOld.vue";
 // import NoorSirehFinal from "@/component/NoorSirehFinal.vue";
 // import TreeExpanderExample from "@/component/TreeExpanderExample.vue";
+import TreeExpanderFinal from "@/component/TreeExpanderFinal.vue";
 // import {graphService} from '@/service/GraphService'
 </script>
 
 <template>
-  <AnsabTreeGraphLayout/>
+  <TreeExpanderFinal/>
 </template>
 
 <style>
---
+++ src/component/TreeExpanderFinal.vue
@@ -0,0 +1,343 @@
+<script setup>
+import go from 'gojs'
+import treeModelData from '../tree/tree2.json'
+import Slider from "primevue/slider";
+import {ref, watch} from "vue";
+
+let graphDiagram
+let timer = null
+const WAIT_TIME = 1000 // 1 s
+let selectedKey = -1
+const nodeCircleSize = 15
+const nodeImageHeight = 48 / 3
+const nodeImageWidth = 42 / 3
+const nodeHeightMargin = 5
+const sliderValue = ref(20)
+const nodeNumber = ref()
+
+// Watch the PrimeVue slider value and update the diagram scale
+watch(sliderValue, (newValue) => {
+  if (graphDiagram)
+    graphDiagram.scale = valueToScale(newValue)
+})
+
+function init() {
+  graphDiagram = new go.Diagram('DiagramDiv', {
+    initialAutoScale: go.AutoScale.UniformToFill,
+    allowMove: false,
+    layout: new go.TreeLayout({
+      angle: 90,
+      nodeSpacing: 5,
+      layerSpacing: 30,
+      arrangement: go.TreeArrangement.FixedRoots
+    })
+  });
+
+  graphDiagram.div.setAttribute('class', 'scroll');
+
+  const tool = graphDiagram.commandHandler;
+
+  tool.decreaseZoom = () => {
+    console.log('my decrease zoom function!');
+    if (graphDiagram.scale < 0.3) return;
+    go.CommandHandler.prototype.decreaseZoom.call(tool);
+  };
+
+  tool.canDecreaseZoom = () => graphDiagram.scale >= 0.3;
+
+  tool.increaseZoom = () => {
+    console.log('my increase zoom function!');
+    if (graphDiagram.scale > 2) return;
+    go.CommandHandler.prototype.increaseZoom.call(tool);
+  };
+
+  tool.canIncreaseZoom = () => graphDiagram.scale <= 2;
+
+  const nodeClicked = (e, obj) => {
+    const key = obj.part.data.key;
+    clickedOnNode(key);
+  };
+
+  const nodeRightClicked = (key, xPos, yPos) => {
+    const node = graphDiagram.findNodeForKey(key);
+    if (node && node.data.haveRelation === 'true') {
+      console.log(node.data.relationIds);
+    }
+  };
+
+  function showContextMenu(obj, diagram) {
+    const {x, y} = diagram.lastInput.viewPoint;
+
+    if (obj) {
+      const key = obj.part.data.key;
+      nodeRightClicked(key, x, y);
+    }
+  }
+
+  graphDiagram.contextMenu = new go.HTMLInfo({
+    show: (obj, diagram) => showContextMenu(obj, diagram)
+  });
+
+  const nodeOpenMenuClicked = (e, obj) => {
+    const {x, y} = graphDiagram.lastInput.viewPoint;
+
+    if (obj) {
+      const node = obj.part;
+      const key = node.data.key;
+      const shape = node.findObject('SHAPE');
+      const textBlock = node.findObject('TextBlock');
+      const h1 = shape ? shape.height : 0;
+      const h2 = 40; // Height for textBlock
+
+      nodeRightClicked(key, x + 5, y + h1 + h2 + nodeHeightMargin);
+    }
+  };
+
+  graphDiagram.nodeTemplate = new go.Node('Vertical', {
+    selectionChanged: nodeSelectionChanged,
+    isTreeExpanded: true, // Initially expand all nodes
+  })
+      .add(
+          new go.Panel('Auto')
+              .add(
+                  new go.Shape({
+                    name: 'SHAPE',
+                    fill: go.Brush.randomColor(),
+                    stroke: 'orange',
+                    strokeWidth: 1
+                  }).bind('fill', '', () => go.Brush.randomColor()),
+                  new go.TextBlock({
+                    font: 'bold 13px Helvetica, bold Arial, sans-serif',
+                    stroke: '#212121',
+                    margin: 3
+                  }).bind('text', 'title')
+              ),
+          // Expand/collapse button with custom click behavior
+          go.GraphObject.make('TreeExpanderButton')
+      )
+
+  graphDiagram.linkTemplate = new go.Link({
+    fromEndSegmentLength: 10,
+    toEndSegmentLength: 10,
+    relinkableFrom: true,
+    relinkableTo: true,
+    routing: go.Routing.Orthogonal,
+    corner: 10,
+  })
+      .add(new go.Shape({strokeWidth: 1}))
+      .add(new go.Shape({
+        toArrow: 'Standard',
+        strokeWidth: 1,
+      }));
+
+  graphDiagram.model = new go.TreeModel({
+    isReadOnly: true,
+    nodeDataArray: treeModelData
+  });
+  syncSliderWithZoomSlider()
+}
+
+function onCenterRoot() {
+  graphDiagram.scale = 1
+  graphDiagram.commandHandler.scrollToPart(graphDiagram.findNodeForKey(1))
+}
+
+function nodeSelectionChanged(_node) {
+  console.log('selection changed.')
+}
+
+function goToNode() {
+  if (!nodeNumber.value) {
+    console.log('Please enter a valid node number.')
+    return
+  }
+  const nodeData = graphDiagram.findNodeForKey(nodeNumber.value)
+
+  // Search for the node in the diagram's model data by 'id'
+  // const nodeDataArray = graphDiagram.model.nodeDataArray
+  // const nodeData = nodeDataArray.find(node => node.id === nodeNumber.value)
+
+  if (nodeData) {
+    graphDiagram.select(nodeData) // Select the node
+    graphDiagram.centerRect(nodeData.actualBounds) // Center on the selected node
+    document.getElementById('nodeNumberInput').focus()
+  }
+  else {
+    console.log('Node not found. Please enter a valid node number.')
+  }
+}
+
+function scaleToValue(scale) {
+  const minScale = 0.1
+  const maxScale = 5
+  const minValue = -50
+  const maxValue = 100
+
+  return ((scale - minScale) / (maxScale - minScale)) * (maxValue - minValue) + minValue
+}
+
+// Convert slider value to scale
+function valueToScale(value) {
+  const minScale = 0.1
+  const maxScale = 5
+  const minValue = -50
+  const maxValue = 100
+
+  return ((value - minValue) / (maxValue - minValue)) * (maxScale - minScale) + minScale
+}
+
+function syncSliderWithZoomSlider() {
+  graphDiagram.addDiagramListener('ViewportBoundsChanged', (_e) => {
+    // calculateOverviewMap() // Listening to viewport bounds changes for overview map zoom and bounds.
+    // Update PrimeVue slider when ZoomSlider changes
+    if (graphDiagram) {
+      const scale = graphDiagram.scale
+      sliderValue.value = scaleToValue(scale)
+    }
+  })
+}
+
+// ---------------------------------------------------------------------------------- Functions for Qt App
+
+function zoomToFit() {
+  graphDiagram.commandHandler.zoomToFit()
+}
+
+function scale(scale) {
+  graphDiagram.scale = scale
+}
+
+function scrollToNode(key) {
+  const node = graphDiagram.findNodeForKey(key)
+  if (node !== null)
+    graphDiagram.scrollToRect(node.actualBounds)
+}
+
+function clickedOnNode(key) {
+  // Iterate over all nodes in the Diagram
+  graphDiagram.nodes.each((node) => {
+    const textBlock = node.findObject('TextBlock');
+    const picture = node.findObject('Picture');
+    const button = node.findObject('Button');
+
+    // Resetting node styles
+    if (textBlock) {
+      textBlock.margin = new go.Margin(0, 0, 0, 10);
+      textBlock.stroke = node.data.haveRelation === 'true' ? '#0402F1' : 'black';
+    }
+
+    if (button) {
+      button.margin = new go.Margin(0, -10, 0, 0);
+    }
+
+    if (picture) {
+      picture.source = '';
+      picture.margin = new go.Margin(0, 0, 0, 0);
+    }
+  });
+
+  const node = graphDiagram.findNodeForKey(key);
+
+  if (node) {
+    const textBlock = node.findObject('TextBlock');
+    const button = node.findObject('Button');
+    const picture = node.findObject('Picture');
+
+    if (textBlock) {
+      textBlock.stroke = '#FF7F7F';
+      textBlock.margin = new go.Margin(0, 5, 0, 0);
+    }
+
+    if (button) {
+      button.margin = new go.Margin(0, 0, 0, 0);
+    }
+
+    if (picture) {
+      picture.source = './assets/images/personIcon___.png';
+      picture.margin = new go.Margin(0, 0, 0, 5);
+    }
+
+    scrollToNode(key);
+  }
+}
+
+function updateGOJSModel() {
+  // Placeholder function for updating the GOJS model
+  // Implementation would depend on the actual data fetching and model update logic
+}
+
+function selectedNode(key) {
+  selectedKey = key;
+}
+
+// --------------------------------------------------------------------
+
+window.addEventListener('DOMContentLoaded', init);
+</script>
+
+<template>
+  <div class="flex flex-column relative h-full">
+    <div
+        class="relative flex justify-content-between align-items-center my-2 mx-2"
+    >
+      <div class="flex align-items-center gap-2">
+        <Button
+            id="zoomToFit"
+            @click="graphDiagram.commandHandler.zoomToFit()"
+        >
+          Zoom to Fit
+        </Button>
+        <Button
+            @click="onCenterRoot()"
+        >
+          Center on root
+        </Button>
+      </div>
+      <div class="flex align-items-center gap-2">
+        <InputNumber
+            v-model="nodeNumber"
+            input-id="minmax-buttons"
+            id="nodeNumberInput"
+            mode="decimal"
+            show-buttons
+            :min="3"
+            :max="7999"
+            fluid
+            class=""
+            type="number"
+            @keyup.enter="goToNode"
+        />
+        <Button
+            class="fetch-button"
+            @click="goToNode"
+        >
+          Find Node
+        </Button>
+        <div class="w-10rem flex flex-column align-items-center gap-2 p-2 border-2 border-gray-300 border-round-sm">
+          <div class="w-8rem py-2">
+            <Slider
+                v-model="sliderValue"
+                class="w-full"
+                :min="-50"
+                :max="100"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="graph-container flex-auto m-2 relative">
+      <div
+          id="DiagramDiv"
+          class="h-full border-1 border-gray-500"
+      />
+      <!--      <div-->
+      <!--          id="overviewDiv"-->
+      <!--          class="absolute top-0 left-0 z-4 border-1 border-gray-700"-->
+      <!--          style="height: 10%; width: 10%"-->
+      <!--      />-->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
